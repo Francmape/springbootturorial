36b2c7761a145a405e6ec06fc8bf0ef3fadbf8b5
Francis Akilimali <francmape@gmail.com>
2022-07-28 17:03:56 +0300

    first commit

---
+++ src/main/java/com/skilciti/springboot/tutorial/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.skilciti.springboot.tutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greetings() {
+        return "Welcome to skilciti Ltd.";
+    }
+}
