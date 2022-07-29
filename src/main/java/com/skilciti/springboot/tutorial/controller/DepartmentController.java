23d1136b4dbe8e4449af4534fa4af62275288e9f
Francis Akilimali <francmape@gmail.com>
2022-07-29 15:25:33 +0300

    Implementing Lombok

--- src/main/java/com/skilciti/springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/com/skilciti/springboot/tutorial/controller/DepartmentController.java
@@ -2,9 +2,12 @@ package com.skilciti.springboot.tutorial.controller;
 
 import com.skilciti.springboot.tutorial.entity.Department;
 import com.skilciti.springboot.tutorial.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -13,34 +16,42 @@ public class DepartmentController {
     @Autowired
     public DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department) {
+    public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("DepartmentController: saveDepartment");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchAllDepartments() {
+        LOGGER.info("DepartmentController: fetchAllDepartments");
         return departmentService.fetchAllDepartments();
     }
 
     @GetMapping("/departments/{id}")
     public Department getDepartmentById(@PathVariable("id") Long departmentId) {
+        LOGGER.info("DepartmentController: getDepartmentById");
         return departmentService.getDepartmentById(departmentId);
     }
 
     @DeleteMapping("/departments/{id}")
     public String deleteDepartment(@PathVariable("id") Long departmentId) {
+        LOGGER.info("DepartmentController: deleteDepartment");
         departmentService.deleteDepartment(departmentId);
         return "Department deleted successfully";
     }
 
     @PutMapping("/departments/{id}")
     public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department) {
+        LOGGER.info("DepartmentController: updateDepartment");
         return departmentService.updateDepartment(departmentId, department);
     }
 
     @GetMapping("/departments/name/{name}")
     public Department fetchDepartmentByName(@PathVariable("name") String departmentName) {
+        LOGGER.info("DepartmentController: fetchDepartmentByName");
         return departmentService.fetchDepartmentByName(departmentName);
     }
 
--- src/main/java/com/skilciti/springboot/tutorial/entity/Department.java
+++ src/main/java/com/skilciti/springboot/tutorial/entity/Department.java
@@ -1,70 +1,40 @@
 package com.skilciti.springboot.tutorial.entity;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @NotBlank(message = "Provide department name")
+    @NotNull
+    /*
+   @Length(max = 30, min = 2)
+    @Email //Validating email
+    @Positive //Validating values
+    @Negative //Validating values
+    @PositiveOrZero //Validating values
+    @NegativeOrZero //Validating values
+    @Future //Validating date
+    @FutureOrPresent //Validating date
+    @Past //Validating date
+    @PastOrPresent //Validating date
+    */
     private String name;
     private String address;
     private String code;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public Department() {
-    }
-
-    public Department(Long id, String name, String address, String code) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.code = code;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", code='" + code + '\'' +
-                '}';
-    }
 }
